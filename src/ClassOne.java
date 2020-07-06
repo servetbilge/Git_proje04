924d865830adc2823cbc3e932aa7080438f9990a
servetbilge <67020144+servetbilge@users.noreply.github.com>
2020-07-06 23:07:09 +0200

    Update ClassOne.java
    değişiklik

--- src/ClassOne.java
+++ src/ClassOne.java
@@ -5,6 +5,7 @@ public class ClassOne {
 		
 		System.out.println("Local masterdan ilk commit");
 		System.out.println("Featureden ilk commit");
+		System.out.println("Featureden ilk commit");
 	}
 
 }
